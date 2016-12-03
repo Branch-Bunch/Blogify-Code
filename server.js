54e307e9f1da99b705a8c5a7d70a1ecbd5c53f37
kirinrastogi <rastogikirin@gmail.com>
2016-12-03 13:00:48 -0500

    Add env, and replit connection

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+const dotenv = require('dotenv').config()
 const ReplitClient = require('replit-client')
+
+const TOKEN = JSON.parse(process.env.TOKEN)
+
+let repl = new ReplitClient('api.repl.it', 80, 'ruby', TOKEN)
+
+repl.connect()
+    .then(() => {
+        console.log('connected') 
+    }, (error) => {
+        console.log('failed')
+        console.log(error)
+    })
