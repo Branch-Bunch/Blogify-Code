d559054916401bbb8d4b19fc98e146bd065c7189
kirinrastogi <rastogikirin@gmail.com>
2016-12-03 13:35:07 -0500

    Testing repl works

--- server.js
+++ server.js
@@ -3,11 +3,20 @@ const ReplitClient = require('replit-client')
 
 const TOKEN = JSON.parse(process.env.TOKEN)
 
-let repl = new ReplitClient('api.repl.it', 80, 'ruby', TOKEN)
+let repl = new ReplitClient('api.repl.it', 80, 'python3', TOKEN)
 
 repl.connect()
     .then(() => {
-        console.log('connected') 
+        console.log('connected')
+        repl.evaluate('x = 1\nprint(x)', {stdout: (out) => {
+           console.log(out) 
+        }})
+            .then((result) => {
+                if (result.error) {
+                    console.log(result.error)
+                }
+                console.log(result.data)
+            })
     }, (error) => {
         console.log('failed')
         console.log(error)
