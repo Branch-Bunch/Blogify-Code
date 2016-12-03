778726a640c40bddd289544034ad566e25d3abca
Jacky Chiu <jacky.chiu888@gmail.com>
2016-12-03 12:38:03 -0500

    Inital commit with boilerplate for front end

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './public/js/index.js',
+    output: {
+        path: __dirname, filename: './public/js/bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+}
