5e1edc5001f5c59ce20d106499765f3fd31a1300
BenEmdon <benjaminemdon@gmail.com>
2016-12-03 19:20:47 -0500

    added MarkdownBlock

--- public/js/components/MarkupBody.js
+++ public/js/components/MarkupBody.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import MarkdownBlock from './MarkdownBlock.js'
 
 export default class MarkupBody extends React.Component {
     render() {
-
+        return (
+            <MarkdownBlock markdown="hi there very **cool**" />
+        )
     }
 }
